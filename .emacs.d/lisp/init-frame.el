2382254485180fabb2fae9623e5f1a7f8b1b5718
WangChengQing <377133665@qq.com>
2019-08-04 04:04:00 +0800

    8.4

--- .emacs.d/lisp/init-frame.el
+++ .emacs.d/lisp/init-frame.el
@@ -20,7 +20,47 @@
 ;; ********************
 ;; mode line
 ;; ********************
-;; 具体见init-nyan.el
+(require 'init-nyan)
+(require 'init-window-number)
+(defun window-number-string ()
+  "Returns the string containing the number of the current window"
+  (let ((n (window-number)))
+    (if (= n 0) ""
+      (propertize
+       (concat " [" (number-to-string n) "] ")))))
+
+;; 屏蔽某些minor-mode
+(defun get-minor-mode-alist ()
+  (dolist (var minor-mode-alist)
+    (let ((first (format "%s" (car var))))
+      (when (or (string= first "company-mode")
+		(string= first "smartparens-mode")
+		(string= first "disable-mouse-mode")
+		(string= first "disable-mouse-global-mode")
+		(string= first "smartparens-mode")
+		(string= first "hs-minor-mode")
+		(string= first "helm-mode")
+		)
+	(delete var minor-mode-alist))))
+  (list minor-mode-alist))
+
+;; 修改mode-line
+(setq-default mode-line-format
+	      (list
+	       "%e"
+	       'mode-line-mule-info
+	       'mode-line-modified
+	       "  "
+	       'mode-line-buffer-identification
+	       "  "
+	       '(:eval (list (nyan-create)))
+	       "%l  "
+	       '(:propertize ("" mode-name))
+	       '(:eval (get-minor-mode-alist))
+	       " %n  "
+	       'mode-line-process
+	       ))
+
 
 ;; ********************
 ;; frame size
--- .emacs.d/lisp/init-nyan.el
+++ .emacs.d/lisp/init-nyan.el
@@ -8,38 +8,4 @@
 
 (setq nyan-minimum-window-width 30)
 
-
-;; 屏蔽某些minor-mode
-(defun get-minor-mode-alist ()
-  (dolist (var minor-mode-alist)
-    (let ((first (format "%s" (car var))))
-      (when (or (string= first "company-mode")
-		(string= first "smartparens-mode")
-		(string= first "disable-mouse-mode")
-		(string= first "disable-mouse-global-mode")
-		(string= first "smartparens-mode")
-		(string= first "hs-minor-mode")
-		(string= first "helm-mode")
-		)
-	(delete var minor-mode-alist))))
-  (list minor-mode-alist))
-
-
-;; 修改mode-line
-(setq-default mode-line-format
-	      (list
-	       "%e"
-	       'mode-line-mule-info
-	       'mode-line-modified
-	       "  "
-	       'mode-line-buffer-identification
-	       "  "
-	       '(:eval (list (nyan-create)))
-	       "%l  "
-	       '(:propertize ("" mode-name))
-	       '(:eval (get-minor-mode-alist))
-	       " %n  "
-	       'mode-line-process
-	       ))
-
 (provide 'init-nyan)
