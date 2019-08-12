cebec56c69052d332a567fe84eb0d664a3bb9078
WCQ <377133665@qq.com>
2019-08-12 17:42:05 +0800

    增加flycheck对c++11支持

--- .emacs.d/lisp/init-flycheck-mode.el
+++ .emacs.d/lisp/init-flycheck-mode.el
@@ -2,6 +2,9 @@
 
 (add-hook 'c-mode-common-hook 'flycheck-mode)
 
+;; c++ mode时候默认使用c++11标准
+(add-hook 'c++-mode-hook (lambda () (setq flycheck-gcc-language-standard "c++11")))
+
 (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
 
 
