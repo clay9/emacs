dc836d24baef2d8d4e2df2a61717bd2bc2962082
WangChengQing <377133665@qq.com>
2019-08-28 20:01:01 +0800

    Auto Push 2019-08-28 20:01:00

---
+++ .emacs.d/elpa/lv-20190821.947/lv-autoloads.el
@@ -0,0 +1,15 @@
+;;; lv-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("lv.el") (23909 11806 260189 19000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; lv-autoloads.el ends here
