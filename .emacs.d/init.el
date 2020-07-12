f9652f95a053fe2b54b4e7fd61185687d459c364
WangChengQing <377133665@qq.com>
2020-07-12 20:01:01 +0800

    Auto Push 2020-07-12 20:01:01

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -91,11 +91,11 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (magit bm figlet yasnippet window-number symbol-overlay sr-speedbar smartparens slime org-bullets org nyan-mode hydra helm-gtags graphviz-dot-mode flycheck expand-region dracula-theme disable-mouse company-c-headers))))
+    (php-mode magit bm figlet yasnippet window-number symbol-overlay sr-speedbar smartparens slime org-bullets org nyan-mode hydra helm-gtags graphviz-dot-mode flycheck expand-region dracula-theme disable-mouse company-c-headers))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-;;(put 'narrow-to-page 'disabled nil)
+(put 'narrow-to-page 'disabled nil)
